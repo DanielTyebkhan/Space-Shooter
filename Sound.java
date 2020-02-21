75d9d45e21d3bd146ed12a640821b9f0fa297537
tyebkhan <30511534+tyebkhan@users.noreply.github.com>
2020-02-20 21:11:33 -0500

    Update Sound.java

--- Sound.java
+++ Sound.java
@@ -3,12 +3,12 @@ import java.net.URL;
 public class Sound {
     URL url;
     AudioClip sound;
-    //sets url
+
     public Sound(String s){
         url = (getClass().getClassLoader().getResource(s));
         sound = Applet.newAudioClip(url);
     }
-    //plays on a new thread so it is not interrupted
+
     public void play(){
         new Thread(){
             public void run(){
