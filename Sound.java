48fd37b63a776507f9b66d1e0ceca6b0fd1ca558
tyebkhan <30511534+tyebkhan@users.noreply.github.com>
2020-03-20 17:52:55 -0700

    docstring and refactor in Sound

--- Sound.java
+++ Sound.java
@@ -4,16 +4,19 @@ public class Sound {
     URL url;
     AudioClip sound;
 
+    /**
+     * Constructor
+     * @param s The filename with the sound
+     */
     public Sound(String s){
         url = (getClass().getClassLoader().getResource(s));
         sound = Applet.newAudioClip(url);
     }
 
+    /**
+     * plays the sound
+     */
     public void play(){
-        new Thread(){
-            public void run(){
-                sound.play();
-            }
-        }.start();
+        new Thread(() -> sound.play()).start();
     }
 }
