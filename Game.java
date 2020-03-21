5da50438f457aeb94e38e2f63d9ad47762b4ccce
tyebkhan <30511534+tyebkhan@users.noreply.github.com>
2020-03-20 17:50:06 -0700

    constants and docstrings in Game

--- Game.java
+++ Game.java
@@ -5,29 +5,49 @@ import java.util.*;
 import java.io.*;
 
 public class Game {
+    public static final String NAME = "Shoot Things";
+    public static final String GAME_OVER = "Game Over";
+    public static final String FINAL_SCORE = "Your Final Score: ";
+    public static final String HIGH_SCORE_MSG = "Highscore: ";
+    public static final String REPLAY_MSG = "Play Again";
+    public static final String NEW_HIGH_SCORE = "Congratulations, You got a new Highscore!";
+    public static final String HIGH_SCORE_FILE = "highscore.txt";
+    public static final String NO_SCORE_FILE_MSG = "Error the Highscorefile is missing";
+
+
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 1000;
+
     static JFrame frame;
     static Background maingame = new Background();
     static JPanel endpanel;
     static int highscore;
     static int gamescore;
+
+    /**
+     * Constructor
+     */
     public Game(){
-        try(Scanner input = new Scanner(new File("highscore.txt"))){
+        try(Scanner input = new Scanner(new File(HIGH_SCORE_FILE))){
             highscore = input.nextInt();
         }catch(FileNotFoundException e){
-            System.out.println("Error the Highscorefile is missing");
+            System.out.println(NO_SCORE_FILE_MSG);
         }
-        frame = new JFrame("Shoot Things");
+        frame = new JFrame(NAME);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.setSize(1000,1000);
+        frame.setSize(WIDTH,HEIGHT);
         frame.add(maingame);
         frame.setVisible(true);
     }
 
     public static void main(String[] args){
-        Game run = new Game();
+        new Game();
     }
 
+    /**
+     * Ends the game
+     */
     public static void endGame(){
         frame.remove(maingame);
         gamescore = maingame.getScore();
@@ -36,28 +56,28 @@ public class Game {
         endpanel.setBackground(Color.black);
         endpanel.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-        JLabel endlabel1 = new JLabel("Game Over");
+        JLabel endlabel1 = new JLabel(GAME_OVER);
         endlabel1.setForeground(Color.green);
         c.gridx = 0;
         c.gridy = 0;
         endpanel.add(endlabel1, c);
-        JLabel endlabel2 = new JLabel("Your Final Score: " + gamescore);
+        JLabel endlabel2 = new JLabel(FINAL_SCORE + gamescore);
         endlabel2.setForeground(Color.green);
         c.gridx = 0;
         c.gridy = 1;
         endpanel.add(endlabel2, c);
-        JLabel endlabel3 = new JLabel("Highscore: " + highscore);
+        JLabel endlabel3 = new JLabel(HIGH_SCORE_MSG + highscore);
         endlabel3.setForeground(Color.green);
         c.gridx = 0;
         c.gridy = 2;
         endpanel.add(endlabel3, c);
-        JButton rbutton = new JButton("Play Again");
+        JButton rbutton = new JButton(REPLAY_MSG);
         class RBListener implements ActionListener{
             @Override
             public void actionPerformed(ActionEvent a){
                 frame.setVisible(false);
                 maingame = new Background();
-                Game run = new Game();
+                new Game();
             }
         }
         rbutton.addActionListener(new RBListener());
@@ -69,15 +89,18 @@ public class Game {
         frame.revalidate();
     }
 
+    /**
+     * Sets the new high score
+     */
     public static void highscore(){
         if(gamescore > highscore){
             //new thread prevents concurrent modification exception
-            new Thread(() -> JOptionPane.showMessageDialog(frame, "Congratulations, You got a new Highscore!")).start();
-            try(PrintWriter out = new PrintWriter("highscore.txt")){
+            new Thread(() -> JOptionPane.showMessageDialog(frame, NEW_HIGH_SCORE)).start();
+            try(PrintWriter out = new PrintWriter(HIGH_SCORE_FILE)){
                 highscore = gamescore;
                 out.println(highscore);
             }catch(FileNotFoundException e){
-                System.out.println("filenotfound");
+                System.out.println(NO_SCORE_FILE_MSG);
             }
         }
     }
